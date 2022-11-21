527007e90c13e59c8f4ddd24866c930aafc3bb97
Bhavana-1234 <61504785+Bhavana-1234@users.noreply.github.com>
2022-11-21 16:07:56 +0530

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -1,1 +1,27 @@
-
+#include<iostream>
+using namespace std;
+int main()
+{
+	string str1,str2;
+	int c1=0,c2=0;
+	cout<<"Enter the first string : ";
+	getline(cin,str1);
+	cout<<"Enter the second string : ";
+	getline(cin,str2);
+	for(int i=0;str2[i]!='\0';i++)
+	{
+		c2++;//finding length of string2
+	}
+	char last_ch=str2[c2-1];//storing last character in the ch
+	for(int i=0;str1[i]!='\0';i++)
+	{
+		if(str1[i]==last_ch)
+		{
+			c1++;
+		}
+	}
+	cout<<" number of occurrences of last character in second string, in the first string : ";
+	cout<<c1;
+	return 0;
+	
+}
